2cecd5ee4421b0a3d86564554bf75752e5f2ca6b
03nouman <naumanmullaji3@gmail.com>
2024-09-12 16:39:19 +0530

    how to create variables in javacript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345;
+let  accountName = "bhuti sharma";
+var  accountEmail = "bhuti@gmail.com";
+accountCity   = "Chiplun";
+let accountStatus;
+
+// note: prefer let and const for cretaing variables, becoz of issues of block and functional scope.
+// console.log(accountId,accountName,accountEmail,accountCity,accountStatus);
+console.table([accountId,accountName,accountEmail,accountCity,accountStatus]);
