727f0f438aa47954a244d4d9928fb91eef794bb4
03nouman <naumanmullaji3@gmail.com>
2024-09-25 08:21:30 +0530

    operations in javascripts

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -45,8 +45,79 @@ let someNumber = 33
 
 let someNumberInString = String(someNumber)
 
-console.log(someNumberInString);
-console.log(typeof someNumberInString );
+// console.log(someNumberInString);
+// console.log(typeof someNumberInString );
+
+// *********************************************** Operations ******************************************************************
+
+let value = 3;
+
+let negValue = - value;
+
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2^3
+// console.log(2%3); modulos or remainder
+
+let str1 = "nouman";
+let str2 = " mullaji"
+
+// console.log(str1 + str2);
+
+// complex operations
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 1);
+// console.log(1 + 2 + "2"); // Bad practice
+
+// console.log((2+3) * 10); // Good practice
+
+// console.log(+true); // => 1
+// console.log(+""); => 0
+// console.log(+"nouman"); => NaN
+
+// prefix and post fix:
+// Syntax: x++ , ++x
+
+
+// let x = 2;
+
+// console.log(x); // =>2
+// console.log(++x); // => 3
+
+// console.log(x); // => 3
+
+// console.log(x++); // => 3
+// console.log(x); // => 4
+
+// Example: postfix increment
+let num = 3
+const num2 = num ++ // here it will first use the value of num and then increase num value
+
+// console.log(`num:${num}, num2:${num2}`);
+
+// Example: prefix increment
+
+let a = 3;
+let b = ++ // here it will first increase value of a and then use the value of a
+
+console.log(`a:${a}, b:${b}`);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
