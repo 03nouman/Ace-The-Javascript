44c86abb942f175497946805431abb02fb199b41
03nouman <naumanmullaji3@gmail.com>
2024-09-25 06:34:12 +0530

    some notes updated in variables chapter

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,14 @@
 const accountId = 12345;
 let  accountName = "bhuti sharma";
 var  accountEmail = "bhuti@gmail.com";
-accountCity   = "Chiplun";
-let accountStatus;
+acountCity   = "Chiplun";
+let accountStatus; // it will return undefined in console.log. becoz if we have have only created memory but not given any value.
 
-// note: prefer let and const for cretaing variables, becoz of issues of block and functional scope.
+// acountId = 5432; -> Not allowed becoz it's a constant vraiable
+
+/*
+prefer not to use var,
+ becoz of issues in  block scope and functional scope.*/
+
 // console.log(accountId,accountName,accountEmail,accountCity,accountStatus);
 console.table([accountId,accountName,accountEmail,accountCity,accountStatus]);
