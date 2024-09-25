214f8214b453b66417d2b7bc44da0de41c80be11
03nouman <naumanmullaji3@gmail.com>
2024-09-25 07:43:39 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,52 @@
+let score = undefined
+
+// console.log(typeof score); //simple
+// console.log(typeof(score)); //In method format
+
+/*
+ Every word comes with paraenthsis are called methods or function in javascript
+*/
+
+// conversion with Number
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// 33 -> 33
+// anbc22 -> NaN
+// treu -> 1 and false -> 0
+// null -> 0
+// undefined -> NaN
+
+/*
+ NaN is also a type of number datatype.
+*/
+
+// conversion with boolean
+
+let isLoggedIn = "nouman";
+
+let isLoggedInInBoolean = Boolean(isLoggedIn)
+
+// console.log(isLoggedInInBoolean);
+
+// so after investigating with numberous values such as,
+// 1 => true
+// 0 => false
+// "" => false
+// "nouman" => true
+
+// conversion with String
+
+let someNumber = 33
+
+let someNumberInString = String(someNumber)
+
+console.log(someNumberInString);
+console.log(typeof someNumberInString );
+
+
+
